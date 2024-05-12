427651b1ae5701ecb026b4a18de93a3db0c651b6
1S4QU3s <159395767+1S4QU3s@users.noreply.github.com>
2024-05-11 22:17:59 -0300

    Add files via upload

---
+++ Operadores_Comparacao.py
@@ -0,0 +1,14 @@
+saldo = 200
+saque = 300
+
+print(saldo==saque)
+
+print(saldo!=saque)
+
+print(saldo>saque)
+
+print(saldo>=saque)
+
+print(saldo<saque)
+
+print(saldo<=saque)
---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = "Isaque"
+idade = 31
+
+nome = "Luana"
+
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+
+print(BRAZILIAN_STATES)
